8b72f6f5f2093749cc8fe5ecfdb9c38bd842a243
SomeFamousPerson <86037191+SomeFamousPerson@users.noreply.github.com>
2024-07-25 12:29:45 +1200

    added level change and tileswapper sprite

---
+++ Flag.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+
+
+
+
+func _on_body_entered(body):
+	print("flag_touched")
+	Global.level += 1
+	if Global.level == 1:
+		get_tree().change_scene_to_file("res://levels/level_2.tscn")
+	
+	
+
+
+
+##Funny cursed vesion of the level changing i made
+## it is an awful bastardised way of doing it but it is funny
+
+#var nextlevel = "res://levels/level_1.tscn"
+#var levelNum = 1
+
+#func _on_body_entered(body):
+	#print("flag touched")
+	##sets global variable of level to one higher
+	#Global.level += 1
+	## sets levelNum to be equal to Global.level, but as a string not an int
+	#levelNum = str(Global.level)
+	##changes the 20th character of the string "res://levels/level_1.tscn" to be equal to the next levels number??????
+	##dont ask why i did this
+	#nextlevel[19] = levelNum
+	#print(nextlevel)
+	##sets the scene to nextlevel (which was mmodified to have the name of the next level)
+	#get_tree().change_scene_to_file(nextlevel)
--- player.gd
+++ player.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -150.0
+const JUMP_VELOCITY = -300.0
 
-var gravity = 100
+var gravity = 250
 
 
 func _physics_process(delta):
@@ -12,11 +12,11 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
