590948a28d5c62d7a87274d924129cbcaee7d8ba
SomeFamousPerson <86037191+SomeFamousPerson@users.noreply.github.com>
2024-07-30 15:14:52 +1200

    made swapper tile placement follow the mouse properly
    TODO: make it fit into grid quares properly

--- levels/world.gd
+++ levels/world.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var mouse = get_global_mouse_position()
 var atlas = Vector2i(0,0)
-var swappers_left = 1
+var swappers_remaining = 1
 var tileswapper = preload("res://tile_swapper.tscn")
 	
 	
@@ -10,12 +10,18 @@ func _physics_process(delta):
 	#place tile-swappers
 	mouse = get_global_mouse_position()
 	if Input.is_action_just_pressed("click"):
-		var cell = Vector2i(mouse)/tilemap.tile_set.tile_size/Vector2i(tilemap.scale)
-		print("world position: ",cell)
-		print ("atlas position: ", tilemap.get_cell_atlas_coords(0, cell))
+		var cell = Vector2i(mouse)/tilemap.tile_set.tile_size#/Vector2i(tilemap.scale)
+		print("cell position: ",cell)
+		var world_pos = tilemap.map_to_local(cell)
+		print("world position: ",world_pos)
+		#print ("atlas position: ", tilemap.get_cell_atlas_coords(0, cell))
 		atlas = Vector2i(tilemap.get_cell_atlas_coords(0, cell))
-		if atlas == Vector2i(-1,-1) and swappers_left <= 1:
+		if atlas == Vector2i(-1,-1) and swappers_remaining <= 1:
 			print("valid spot")
 			var swapper = tileswapper.instantiate()
 			add_child(swapper)
-		
+			swapper.top_level = true
+			swapper.position = world_pos
+			print("placed location: ", swapper.position)
+
+
--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -300.0
 var gravity = 250
 
 
