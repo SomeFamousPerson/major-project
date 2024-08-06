4388c9132f93c962e7b7fe3136b299350bdd0d4a
SomeFamousPerson <86037191+SomeFamousPerson@users.noreply.github.com>
2024-08-06 15:14:19 +1200

    made bridges work well

--- tile_swapper.gd
+++ tile_swapper.gd
@@ -2,32 +2,30 @@ extends Node2D
 const RANGE = 3
 var myLocation = Vector2(0,0) 
 var tilemap
+var mouse_in = false
 
 func _ready():
 	print(tilemap)
 	var targets = [Vector2i(myLocation+Vector2i(1,0)),Vector2i(myLocation+Vector2i(2,0)),Vector2i(myLocation+Vector2i(3,0))]
 	print(targets)
-	if tilemap.get_cell_atlas_coords(0,targets[1]) != Vector2i(0,4):
-		##TODO: MAKE THE SET_CELL WORK
-		tilemap.set_cell(0,targets[1],0)
-	pass
+	for i in RANGE:
+		if tilemap.get_cell_atlas_coords(0,targets[i]) == Vector2i(-1,-1):
+			tilemap.set_cell(0,targets[i],1,Vector2i(0,4))
+		else:
+			break
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("click") and mouse_in == true:
+		tilemap.set_cell(0,myLocation,1,Vector2i(-1,-1))
+		#TODO: DELETE SQUARES THAT GOT ADDED
+		#SHOULD BE PRETTY EASY BYUT DONT HAVE TIME NOW
+		#queue_free()
 
 
+func _on_area_2d_mouse_entered():
+	mouse_in = true
+	print("mouse_in")
 
-#func _physics_process(delta):
-	#print(tilemap)
-	#var mouse = get_global_mouse_position()
-	#if Input.is_action_just_pressed("click"):
-		#var cell = Vector2i(mouse)/tilemap.tile_set.tile_size/Vector2i(tilemap.scale)
-		#print("world position: ",cell)
-		#print ("atlas position: ", tilemap.get_cell_atlas_coords(0, cell))
-		#if tilemap.get_cell_atlas_coords(0, cell) == Vector2i(0,0):
-			#print("black")
-			#tilemap.set_cell(0,cell,0,Vector2(1,0))
-		#elif tilemap.get_cell_atlas_coords(0, cell) == Vector2i(1,0):
-			#print("white")
-			#tilemap.set_cell(0,cell,0,Vector2(-1,-1))
-			##tilemap.set_cell(0,cell,0,Vector2(0,1))
-		#else:
-			#print("empty")
-			#tilemap.set_cell(0,cell,0,Vector2(0,0))
+func _on_area_2d_mouse_exited():
+	mouse_in = false
+
