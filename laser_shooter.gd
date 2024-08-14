3816dc6d4222b5a5fabdf5eda2d61616f01bcbc9
SomeFamousPerson <86037191+SomeFamousPerson@users.noreply.github.com>
2024-08-14 19:13:46 +1200

    laser tings

--- laser_shooter.gd
+++ laser_shooter.gd
@@ -1,21 +1,21 @@
 extends StaticBody2D
 var world
 var laser = preload("res://laser.tscn")
-var RANGE = 10
+var RANGE = 4
 
 
-func _ready():
+func _physics_process(delta):
 	if Global.level == 0:
 		world =  %TileMap
 		#gets the tilemap grid position of the square the laser shooter is in
-		var cell = Vector2i(position)/world.tile_set.tile_size/Vector2i(world.scale)
-		var new_laser = laser.instantiate()
-		var atlas = Vector2i(world.get_cell_atlas_coords(0, cell))
-		for i in RANGE:
+		for i in RANGE-1:
+			var cell = Vector2i(position-Vector2((i+1)*14,0))/world.tile_set.tile_size/Vector2i(world.scale)
+			var atlas = Vector2i(world.get_cell_atlas_coords(0, cell))
 			if atlas == Vector2i(-1,-1):
+				var new_laser = laser.instantiate()
+				new_laser.position = cell+Vector2i(-1,(i+1)*16)
 				add_child(new_laser)
-				print(i)
-				new_laser.position = cell+Vector2i(-1,(i*16))
-#
-func _physics_process(delta):
-	pass
+				#print(i)
+			else:
+				print("abcdddd")
+
