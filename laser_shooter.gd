87d0f2b7e22a06d6a99a920742f90c9d0f934e63
SomeFamousPerson <86037191+SomeFamousPerson@users.noreply.github.com>
2024-08-29 13:24:19 +1200

    identified a problem but couldnt solve it
    reason lasers dont work is in line 20 (			cell = Vector2i(position-Vector2((i+1)*14,0))/world.tile_set.tile_size/Vector2i(world.scale)
    ), which is not changing for every iteration of i for some reason

--- laser_shooter.gd
+++ laser_shooter.gd
@@ -2,33 +2,35 @@ extends StaticBody2D
 var world
 var laser = preload("res://laser.tscn")
 var RANGE = 4
-
+var cell
 
 func _physics_process(delta):
 	
-	#var children = $laserbeams.get_children()
-	##print(children)
-	#for c in children:
+	var children = $laserbeams.get_children()
+	#print(children)
+	for c in children:
 		#print("children: ",c)
-		#$laserbeams.remove_child(c)
+		c.queue_free()
 		#self.queue_free()
 
-	if Global.level == 0:
-		world =  %TileMap
-		#gets the tilemap grid position of the square the laser shooter is in
-		for i in RANGE-1:
-			var cell = Vector2i(position-Vector2((i+1)*14,0))/world.tile_set.tile_size/Vector2i(world.scale)
+
+	world =  %TileMap
+	#gets the tilemap grid position of the square the laser shooter is in
+	for i in RANGE-1:
+			cell = Vector2i(position-Vector2((i+1)*14,0))/world.tile_set.tile_size/Vector2i(world.scale)
+#			print(cell)
 			var atlas = Vector2i(world.get_cell_atlas_coords(0, cell))
-			if atlas == Vector2i(-1,-1):
+			#print(atlas)
+			if atlas != Vector2i(0,4):
 				var new_laser = laser.instantiate()
 				new_laser.position = cell+Vector2i(-1,(i+1)*16)
-				add_child(new_laser)
+				$laserbeams.add_child(new_laser)
+			else:
+				print("no child")
 
 				#var new_node2d = Node2D.new()
 				#add_child(new_node2d)
 				#new_node2d.name = "laserbeams"
 				#$laserbeams.add_child(new_laser)
 				
-			else:
-				print("abcdddd")
 
